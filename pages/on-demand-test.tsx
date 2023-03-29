e234fd349700afcf9cd19773aefbcfe1bab2a72f
marissync <maristela@sync.com.br>
2023-03-29 15:47:00 -0300

    on-demand-test using mdx compile & run

--- pages/on-demand-test.tsx
+++ pages/on-demand-test.tsx
@@ -1,54 +1,54 @@
-import { useState, useEffect, Fragment } from 'react'
+import { useState, useEffect, Fragment } from "react";
 //import * as runtime from 'react/jsx-runtime' // Production.
-import * as runtime from 'react/jsx-dev-runtime' // Development.
-import { compile, run } from '@mdx-js/mdx'
+import * as runtime from "react/jsx-dev-runtime"; // Development.
+import { compile, run } from "@mdx-js/mdx";
 import Component1 from "../components/Component1";
-import type { MDXModule } from 'mdx/types'
-// import { MDXProvider } from '@mdx-js/react';
-import remarkFrontmatter from 'remark-frontmatter';
-import path from 'path';
-// const useMDXComponents = {
-//     Component1
-// }
+import type { MDXModule } from "mdx/types";
+import remarkFrontmatter from "remark-frontmatter";
+
 const stringMDX = `---
-Component1:
- name: Mdx Rocks
+ct1:
+    name: Mdx Rocks
 ---
-<Component1 name={props.Component1.name} />
+
+<Component1 name={props.ct1.name} />
 `;
 export default function Page({ code }: any) {
-    const [mdxModule, setMdxModule] = useState<MDXModule>()
-    const Content = mdxModule ? mdxModule?.default : Fragment
-    useEffect(() => {
-        ; (async () => {
-            setMdxModule(await run(code, runtime))
-        })()
-    }, [code]);
-    
-    const pageProps = { Component1: { name: "slkdjfksldfj" } };
+  const [mdxModule, setMdxModule] = useState<MDXModule>();
+  const Content = mdxModule ? mdxModule?.default : Fragment;
+  useEffect(() => {
+    (async () => {
+      setMdxModule(await run(code, runtime));
+    })();
+  }, [code]);
 
-    return (
-        <Content components={{Component1}} {...pageProps}/>
-    )
+  const pageProps = { ct1: { name: "Mdx Rocks" } };
+
+  return (
+    <>
+      <h1>Something about MDX:</h1>
+      <Content components={{ Component1 }} {...pageProps} />
+    </>
+  );
 }
 export async function getStaticProps() {
-    const remarkPlugins = [remarkFrontmatter];
-    const url = path.join(process.cwd(), "components", "index");
-    console.log(url);
-    const code = String(
-        await compile(stringMDX, {
-            outputFormat: 'function-body',
-            development: process.env.NODE_ENV === 'production' ? false : true,
-            remarkPlugins: remarkPlugins,
-            // useDynamicImport: true,
-            // baseUrl: `${url}`
-            //useDynamicImport: true,           
-            // ^-- Generate code for production.
-            // `false` if you use `/jsx-runtime` on client, `true` if you use
-            // `/jsx-dev-runtime`.
-            /* …otherOptions */
-        })
-    )
-    // console.log("code", code);
-    return { props: { code } }
+  //const url = path.join(process.cwd(), "components", "index");
+  //console.log(url);
+  const code = String(
+    await compile(stringMDX, {
+      // jsx: true,
+      outputFormat: "function-body",
+      development: process.env.NODE_ENV === "production" ? false : true,
+      remarkPlugins: [remarkFrontmatter],
+      // useDynamicImport: true,
+      // baseUrl: `${url}`
+      //useDynamicImport: true,
+      // ^-- Generate code for production.
+      // `false` if you use `/jsx-runtime` on client, `true` if you use
+      // `/jsx-dev-runtime`.
+      /* …otherOptions */
+    })
+  );
+  // console.log("code", code);
+  return { props: { code } };
 }
